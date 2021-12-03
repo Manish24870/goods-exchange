161c02ba34d8bc68ede94424b5cf0ce6be0702d2
Manish Acharya <manish24870@gmail.com>
2021-12-03 22:18:46 +0545

    configured redux and its store

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import { ThemeProvider } from "@mui/material";
 import theme from "./utils/materialUI/theme";
 
 import Layout from "./components/layout/Layout";
+import store from "./store";
 import "./App.css";
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <ThemeProvider theme={theme}>
-                <Layout />
-            </ThemeProvider>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>
+                    <Layout />
+                </ThemeProvider>
+            </BrowserRouter>
+        </Provider>
     );
 };
 
